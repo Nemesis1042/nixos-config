db9f9c64b80828e8c19fbea7493a3242d8d421b9
Nemesis1042 <niklashardwig5@gmail.com>
2025-03-05 09:46:51 +0100

    added scripts

--- modules/home/zsh/zsh_alias.nix
+++ modules/home/zsh/zsh_alias.nix
@@ -44,6 +44,8 @@
 
       install = "$HOME/add_pkg.sh";
       prm = "$HOME/remove_pkg.sh";
+      findpkgs="$HOME/find_all_pkgs.sh";
+      listpkgs="$HOME/list_installed_pkgs.sh";
 
       # python
       piv = "python -m venv .venv";
