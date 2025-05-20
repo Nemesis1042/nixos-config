6be1a1af01fa1364f39be5189e44b898a33ee6be
Nemesis1042 <niklashardwig5@gmail.com>
2025-05-20 13:55:43 +0200

    nvim angepasst

--- modules/home/nvim/init.lua
+++ modules/home/nvim/init.lua
@@ -1,3 +1,7 @@
+local config_path = vim.fn.stdpath('config')
+package.path = config_path .. '/?.lua;' .. config_path .. '/?/init.lua;' .. package.path
+
+
 require("plugins")
 require("lsp")
 require("ui")
