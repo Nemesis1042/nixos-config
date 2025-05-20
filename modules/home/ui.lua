6551835a0fa39019ab0b8ece02fdc023b2925bc2
Nemesis1042 <niklashardwig5@gmail.com>
2025-05-20 13:56:06 +0200

    nvim angepasst

---
+++ modules/home/ui.lua
@@ -0,0 +1,6 @@
+-- Kann optional sein – z. B. UI-spezifische Konfigurationen
+-- Du kannst diesen leerlassen oder z. B. IBL oder andere UI-Elemente reinlegen
+-- Beispiel:
+vim.o.relativenumber = true
+vim.o.number = true
+
